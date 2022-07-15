8c1d7a31017376ed00e072637e09dd982b4a90c5
cheenaze <81647544+cheenaze@users.noreply.github.com>
2022-07-15 18:09:37 +0300

    Add files via upload

--- createProject.sh
+++ createProject.sh
@@ -2,11 +2,15 @@
 mkdir $1;
 cd $1;
 mkdir include src build;
-
 echo "\n\nint main(){}" > main.cpp;
-echo "default:\n\tg++ src/*.cpp main.cpp -o build/$1" > Makefile;
-echo "debug:\n\tg++ src/*.cpp main.cpp -Og -o build/$1debug" >> Makefile;
-echo "opt:\n\tg++ src/*.cpp main.cpp -O3 -o build/$1opt" >> Makefile;
+
+echo "compiler := g++" > Makefile
+echo "files := src/*.cpp main.cpp" >> Makefile
+echo "flags := -std=c++2b" >> Makefile
+
+echo "default:\n\t\$(compiler) \$(files) \$(flags) -o build/$1" >> Makefile;
+echo "debug:\n\t\$(compiler) \$(files) \$(flags) -Og -o build/$1debug" >> Makefile;
+echo "opt:\n\t\$(compiler) \$(files) \$(flags) -O3 -o build/$1opt" >> Makefile;
 
 wget -bq -O create.sh https://raw.githubusercontent.com/cheenaze/useful_files/main/create.sh > /dev/null
 chmod +x create.sh;
